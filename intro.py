20eb086965ceb8477e0faa2ceafee6b867c3870e
nefersillo <adrian.aguinaga@uabc.edu.mx>
2024-08-29 19:35:26 -0700

    first commit

---
+++ intro.py
@@ -0,0 +1,37 @@
+print("hello from python")
+# console.log("hello from python);
+
+# let variable = 1;
+variable = 1
+variable = True
+variable = "hi"
+
+array  = ["adrian", "Agunaga", 4, 5]
+print(array[1])
+array.append(6)
+print(array)
+array.pop()
+print(array)
+
+dict = {
+    "name":"adrian",
+    "last_name":"Aguinaga"    
+      }
+
+print(dict)
+
+# for(i=0; i<size; i++){
+#  let temp = array[i]
+#  console.log("temp")}
+
+for numbers in array:
+    print ("adrian")
+    
+# fuction print(){}
+def hello():
+    print("hello from python function")
+
+hello()
+
+
+
